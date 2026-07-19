07da2176ab6dc3dc21e10a0b400613c91cafa90a
sayagatahu1-commits <sayagatahu1@gmail.com>
2026-07-19 20:49:17 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@ from telegram import Update
 from telegram.ext import Application, CommandHandler, ContextTypes
 
 BOT_TOKEN = os.getenv("8686470947:AAHEhEuZ1PN2wDZzCFpHgjIcbcx6ezB9nPo")
-PRIVATE_KEY = os.getenv("0x4cb91ae51c9f26e961ce3f7d0410a3091a3b2c6c16e1e067b234bf47dd93be42").strip()
+PRIVATE_KEY = os.getenv("0x4cb91ae51c9f26e961ce3f7d0410a3091a3b2c6c16e1e067b234bf47dd93be42")
 RPC_URL = "https://omni-testnet.blastapi.io/1b3c4d2e-5f6a-7b8c-9d0e-1f2a3b4c5d6e"
 BRIDGE = Web3.to_checksum_address("0x2D6e44f44A83D5B99BC0745f10d1C4b8BFFF0e7d")
 
