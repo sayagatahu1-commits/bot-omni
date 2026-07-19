da13f08140998a74dcdbb4d9d355ea970f158a4c
sayagatahu1-commits <sayagatahu1@gmail.com>
2026-07-19 20:54:51 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,11 +1,16 @@
 import os
 from web3 import Web3
-from telegram import Update
-from telegram.ext import Application, CommandHandler, ContextTypes
 
 BOT_TOKEN = os.getenv("8686470947:AAHEhEuZ1PN2wDZzCFpHgjIcbcx6ezB9nPo")
 PRIVATE_KEY = os.getenv("0x4cb91ae51c9f26e961ce3f7d0410a3091a3b2c6c16e1e067b234bf47dd93be42")
+print(f"BOT_TOKEN ada gak: {BOT_TOKEN is not None}") # buat debug
+print(f"PRIVATE_KEY ada gak: {PRIVATE_KEY is not None}") # buat debug
+
+if not PRIVATE_KEY:
+    raise ValueError("PRIVATE_KEY KOSONG! Cek Variables Railway")
+
 RPC_URL = "https://omni-testnet.blastapi.io/1b3c4d2e-5f6a-7b8c-9d0e-1f2a3b4c5d6e"
+# ... kode sisanya
 BRIDGE = Web3.to_checksum_address("0x2D6e44f44A83D5B99BC0745f10d1C4b8BFFF0e7d")
 
 w3 = Web3(Web3.HTTPProvider(RPC_URL))
